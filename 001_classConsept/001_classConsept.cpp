6965932745fbb1ef926aa62dd621bcd5196d6638
Onur ALACA <onuralcaa@gmail.com>
2024-03-15 23:30:56 +0300

    Member variables and functions

---
+++ 001_classConsept/001_classConsept.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+#include "studentClass.h"
+
+int main()
+{
+    Student student1; //Student sınıfından bir nesne üretilmesi
+    Student student2; //nesne 2
+
+    student1.studentName = "Onur";
+    student1.studentSurname = "ALACA";
+    student1.studentID = "131";
+    student1.displayInformation();
+
+
+    student2.studentName = "Osman";
+    student2.studentSurname = "YILMAZ";
+    student2.studentID = "314";
+    student2.displayInformation();
+
+    return 0;
+}
---
+++ 001_classConsept/studentClass.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+
+	string studentName, studentSurname, studentID; //üye deðiþkenler
+	
+	void displayInformation(); //üye fonksiyon
+
+
+};
+
+void Student::displayInformation()
+{
+	cout << "Name:" << studentName << " " << studentSurname << endl;
+	cout << "ID:" << studentID << endl;
+}
