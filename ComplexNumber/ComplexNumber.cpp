48c6064a2ac6419c636c0a6e7a221fa9bd395202
Onur ALACA <onuralcaa@gmail.com>
2024-03-22 19:56:02 +0300

    Basic functions of complex numbers were tested.

--- ComplexNumber/ComplexNumber.cpp
+++ ComplexNumber/ComplexNumber.cpp
@@ -1,8 +1,26 @@
 ﻿
 
 #include <iostream>
+#include "complexNumber.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	complexNumber number1(4.5);
+	cout << "Real: " << number1.getReal()<<endl;
+	cout << "Imag : " << number1.getImag()<<endl;
+	number1.displayComplex();
+
+	complexNumber number2(3, -6);
+	cout << "Real: " << number2.getReal() << endl;
+	cout << "Imag : " << number2.getImag() << endl;
+	number2.displayComplex();
+
+	complexNumber number3(number2);
+	number3.displayComplex();
+	number3.setValue(4, -7);
+	number3.displayComplex();
+
+
+
+	return 0;
 }
--- ComplexNumber/complexNumber.h
+++ ComplexNumber/complexNumber.h
@@ -11,8 +11,15 @@ public:
 		complexNumber(int r = 0, int i = 0);
 	~complexNumber();
 	complexNumber(const complexNumber& oth);
-	int getReal() const;
-	int getImag() const;
+	//const olmasýnýn sebebi inputtan baðýmsýz çalýþmasý.
+	int getReal() const
+	{
+		return real;
+	}
+	int getImag() const
+	{
+		return imag;
+	}
 	void setValue(int r = 0, int i = 0);
 	complexNumber addComplex(const complexNumber& oth);
 	complexNumber multiplyComplex(const complexNumber& oth);
@@ -21,4 +28,40 @@ public:
 	void multiplyMinus();
 	void displayComplex();
 };
+
+complexNumber::complexNumber(int r, int i)
+{
+	real = r;
+	imag = i;
+	cout << "Complex number created." << endl;
+
+}
+//4 + 5i (example)
+
+complexNumber::~complexNumber()
+{
+	cout << "Complex number destroyed." << endl;
+}
+
+complexNumber::complexNumber(const complexNumber& oth)
+{
+	real = oth.real;
+	imag = oth.imag;
+	cout << "Complex number copied to target." << endl;
+}
+
+void complexNumber::setValue(int r, int i)
+{
+	real = r;
+	imag = i;
+}
+
+void complexNumber::displayComplex()
+{
+	imag >= 0 ? cout << real << "+" << imag << "i" << endl : cout << real << imag << "i" << endl; //kýsa if-else yapýsý
+}
+
+
+
+
 
