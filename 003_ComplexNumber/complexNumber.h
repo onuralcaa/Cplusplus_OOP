31037b171303a8785b9693604b3d23907d28575d
Onur ALACA <onuralcaa@gmail.com>
2024-04-18 19:16:49 +0300

    Update files

---
+++ 003_ComplexNumber/complexNumber.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class complexNumber {
+private:
+		int real, imag;
+public:
+		complexNumber(int r = 0, int i = 0);
+	~complexNumber();
+	complexNumber(const complexNumber& oth);
+	//const olmasýnýn sebebi inputtan baðýmsýz çalýþmasý.
+	int getReal() const
+	{
+		return real;
+	}
+	int getImag() const
+	{
+		return imag;
+	}
+	void setValue(int r = 0, int i = 0);
+	complexNumber addComplex(const complexNumber& oth);
+	complexNumber multiplyComplex(const complexNumber& oth);
+	complexNumber subtractComplex(complexNumber& oth);
+	complexNumber multiplyConstant(int constant);
+	void multiplyMinus();
+	void displayComplex();
+};
+
+complexNumber::complexNumber(int r, int i)
+{
+	real = r;
+	imag = i;
+	cout << "Complex number created." << endl;
+
+}
+//4 + 5i (example)
+
+complexNumber::~complexNumber()
+{
+	cout << "Complex number destroyed." << endl;
+}
+
+complexNumber::complexNumber(const complexNumber& oth)
+{
+	real = oth.real;
+	imag = oth.imag;
+	cout << "Complex number copied to target." << endl;
+}
+
+void complexNumber::setValue(int r, int i)
+{
+	real = r;
+	imag = i;
+}
+
+void complexNumber::displayComplex()
+{
+	imag >= 0 ? cout << real << "+" << imag << "i" << endl : cout << real << imag << "i" << endl; //kýsa if-else yapýsý
+}
+
+complexNumber complexNumber::addComplex(const complexNumber& oth)
+{
+	complexNumber result;
+	result.real = real + oth.real;
+	result.imag = imag + oth.imag;
+
+	return result;
+}
+//karmaþýk sayý toplama formülü kullanýldý.
+
+complexNumber complexNumber::subtractComplex(complexNumber& oth)
+{
+	complexNumber result;
+	oth.multiplyMinus(); //eksi alma fonksiyonu
+	result = addComplex(oth);
+	return result;
+}
+
+void complexNumber::multiplyMinus()
+{
+	real *= -1;
+	imag *= -1;
+}
+
+complexNumber complexNumber::multiplyConstant(int constant)
+{
+	complexNumber result;
+	result.real = real * constant;
+	result.imag = imag * constant;
+
+	return result;
+}
+//3*(4+5i)= 12 + 15i sonucunu vermeli.
+
+complexNumber complexNumber::multiplyComplex(const complexNumber& oth)
+{
+	complexNumber result;
+	//Hazýr förmül kullanýldý.
+	result.real = real * oth.real - imag * oth.imag;
+	result.imag = real * oth.imag - imag * oth.real;
+	return result;
+}
---
+++ 004_Arrays/class_Concept.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Arrays.h"
+
+int main()
+{
+	Arrays  mainArray;
+
+
+
+
+
+
+
+
+
+	return 0;
+}
