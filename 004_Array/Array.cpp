e97ffff7196584ea1d7d452405af85e6f110674f
Onur ALACA <onuralcaa@gmail.com>
2024-04-18 19:31:26 +0300

    Section 1 completed.

--- 004_Array/Array.cpp
+++ 004_Array/Array.cpp
@@ -4,6 +4,19 @@
 
 
 int main()
-{
-    Array mainArray;
+{ 
+    Array mainArray(10); //default değerle oluştu.
+
+    Array mainArray2(mainArray);
+
+    cout << "Capacity :" << mainArray.getCapacity()<<endl;
+    cout << "Size :" << mainArray.getSize()<<endl;
+
+
+    cout << "Capacity :" << mainArray2.getCapacity() << endl;
+    cout << "Size :" << mainArray2.getSize() << endl;
+
+
+
+    return 0;
 }
